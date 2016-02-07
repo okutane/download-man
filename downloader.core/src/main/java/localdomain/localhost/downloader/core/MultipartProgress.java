827ae9b3f09a3f650d1c2208c6519fe9b568b855
Dmitry Matveev <dmitriy.matveev@corp.mail.ru>
2016-02-07 13:06:04 +0300

    MultipartProgress

---
+++ downloader.core/src/main/java/localdomain/localhost/downloader/core/MultipartProgress.java
@@ -0,0 +1,64 @@
+package localdomain.localhost.downloader.core;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ * @author <a href="mailto:dmitriy.matveev@odnoklassniki.ru">Dmitriy Matveev</a>
+ */
+public class MultipartProgress {
+    private final TreeSet<ProgressPart> parts = new TreeSet<>();
+    private final long size;
+
+    public MultipartProgress(long size) {
+        this.size = size;
+    }
+
+    public double getProgress() {
+        return parts.stream().mapToLong(p -> p.to - p.from).sum() / (double)size;
+    }
+
+    public synchronized void addProgress(long offset, long length) {
+        long from = offset;
+        long to = offset + length;
+
+        Iterator<ProgressPart> iterator = parts.iterator();
+        while (iterator.hasNext()) {
+            ProgressPart part = iterator.next();
+
+            if (from <= part.from && to >= part.to) {
+                // overwriting existing progress
+                iterator.remove();
+            } else if (from >= part.from && to <= part.to) {
+                // useless progress
+                return;
+            } else if (from <= part.from && to >= part.from) {
+                // partial overwrite or merge
+                to = part.to;
+                iterator.remove();
+            } else if (part.from <= from && part.to >= from) {
+                // partial overwrite or merge
+                from = part.from;
+                iterator.remove();
+            }
+        }
+
+        // adding new or merged part
+        parts.add(new ProgressPart(from, to));
+    }
+
+    private static class ProgressPart implements Comparable<ProgressPart> {
+        private final long from;
+        private final long to;
+
+        public ProgressPart(long from, long to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public int compareTo(ProgressPart o) {
+            return Long.compare(from, o.from);
+        }
+    }
+}
---
+++ downloader.core/tests/localdomain/localhost/downloader/core/MultipartProgressTest.java
@@ -0,0 +1,55 @@
+package localdomain.localhost.downloader.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="mailto:dmitriy.matveev@odnoklassniki.ru">Dmitriy Matveev</a>
+ */
+public class MultipartProgressTest {
+    @Test
+    public void testNew() {
+        MultipartProgress progress = new MultipartProgress(200);
+        assertEquals(0.0, progress.getProgress(), 0.01);
+    }
+
+    @Test
+    public void testPartial() {
+        MultipartProgress progress = new MultipartProgress(200);
+        progress.addProgress(0, 50);
+        assertEquals(0.25, progress.getProgress(), 0.01);
+    }
+
+    @Test
+    public void testOverlapping() {
+        MultipartProgress progress = new MultipartProgress(200);
+        progress.addProgress(50, 100);
+        progress.addProgress(0, 100);
+        assertEquals(0.75, progress.getProgress(), 0.01);
+    }
+
+    @Test
+    public void testComplete() {
+        MultipartProgress progress = new MultipartProgress(200);
+        progress.addProgress(100, 100);
+        progress.addProgress(0, 100);
+        assertEquals(1.0, progress.getProgress(), 0.01);
+    }
+
+    @Test
+    public void testAlreadyRecordedProgress() {
+        MultipartProgress progress = new MultipartProgress(200);
+        progress.addProgress(50, 100);
+        progress.addProgress(75, 50);
+        assertEquals(0.5, progress.getProgress(), 0.01);
+    }
+
+    @Test
+    public void testBiggerProgress() {
+        MultipartProgress progress = new MultipartProgress(200);
+        progress.addProgress(75, 50);
+        progress.addProgress(50, 100);
+        assertEquals(0.5, progress.getProgress(), 0.01);
+    }
+}
